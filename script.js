5e475724e64f692f94b09d65acbd4d172c2fe664
Ozias <131364714+AnjosOzias@users.noreply.github.com>
2026-02-27 11:33:38 -0300

    FINAL DESIGN SCRIPT
    I've finished coding the event that draws on the grid.

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ const colunas = 16;
 
 for (let i = 0; i < total; i++) {
   const div = document.createElement("div");
-  div.className = "item";
+  div.setAttribute("class","item");
+  //div.className = "item";
   //div.textContent = i + 1;
 
   // Calcula largura proporcional às colunas
   div.style.flex = `0 0 calc(100% / ${colunas} - 5px)`;
-
   container.appendChild(div);
+
+    div.addEventListener("mouseover",(evt)=>{
+    console.log(evt)
+    div.classList.add('itemn')
+})
 }
+

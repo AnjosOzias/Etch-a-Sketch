04d32f6f20d981a1c4ec3beb36b38e549726448b
Ozias <131364714+AnjosOzias@users.noreply.github.com>
2026-03-01 13:28:12 -0300

    I finalized the loop for the RGB.
    I added a loop to make the event increase the opacity with each mouse iteration.

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 const container = document.getElementById("container");
+const btn = document.querySelector(".btn");
 
-const total = 256;   
-const colunas = 16;
+btn.addEventListener("click",(evt)=>{
+    const colunas = Number(prompt("Digite um número"));
+    
+    if(colunas > 100){
+        alert("Digite um número até cem");
+        return;
+    }
+    if(isNaN(colunas)){
+        alert("digite um número válido");
+        return;
+    }
+    if(!colunas){
+        alert("UNDEFINED: Digite um número até cem");
+        return;
+    }
+
+    total = colunas*colunas;
+
+  container.replaceChildren(); 
+
 
 for (let i = 0; i < total; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("class","item");
+    const div = document.createElement("div");
+    div.setAttribute("class","item");
   //div.className = "item";
   //div.textContent = i + 1;
+    div.style.flex = `1 0 calc(100% / ${colunas} - 3px)`;
+    container.appendChild(div);
+    const r = Math.floor(Math.random()*256)
+    const g = Math.floor(Math.random()*256)
+    const b = Math.floor(Math.random()*256)
 
-  // Calcula largura proporcional às colunas
-  div.style.flex = `0 0 calc(100% / ${colunas} - 5px)`;
-  container.appendChild(div);
+    let op = 0;
 
     div.addEventListener("mouseover",(evt)=>{
     console.log(evt)
-    div.classList.add('itemn')
+
+    op += 10;
+    if (op > 100) op = 100;
+    div.style.backgroundColor = `rgb(${r}, ${g}, ${b}, ${op}%)`
+    console.log(op)
+
 })
 }
-
+})
